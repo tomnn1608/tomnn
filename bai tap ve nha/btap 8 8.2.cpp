9e40acd27f0993673d4f83fe1ce2a43c24532c8b
tomnn1608 <nxt16082004@gmail.com>
2022-08-08 16:47:09 +0700

    btap S=...+n

---
+++ bai tap ve nha/btap 8 8.2.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+      int main(){
+      	int n;
+      	printf("nhap so n cuoi=");
+      	scanf("%d",&n);
+      	int S;
+      	S=0;
+      	int m=0;
+      	while(m<=n){
+      	S=S+m;
+      	m++;
+      	}
+      	printf("Tong =%d",S);
+  
+}
+      		
+      		
+      		
+      		
+		  
+      	
+      	
+      	
+      	
+      	
+      	
+      	
+      	
+      	
+      	
+	  
